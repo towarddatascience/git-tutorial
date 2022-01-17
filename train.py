3e1faaa1270ade266034669f7d4dfb4691679908
towarddatascience <towarddatascience@gmail.com>
2022-01-17 23:00:54 +0700

    update train.py

--- train.py
+++ train.py
@@ -1,5 +1,16 @@
 from torch.nn import Model
-class Model():
+
+class Model1():
+    def __init__(self, hidden_size, n_layers, lr):
+        self.hidden_size = hidden_size
+        self.n_layers = n_layers
+	self.lr = lr
+        self.model = Model(self.n_layers, self.hidden_size)
+    def fit(self, X, y):
+        self.model.fit(X, y)
+
+
+class Model2():
     def __init__(self, hidden_size, n_layers):
 	self.hidden_size = hidden_size
  	self.n_layers = n_layers
