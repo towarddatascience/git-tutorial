709daffb429abb7d50031c6478a48398b0a2f155
towarddatascience <towarddatascience@gmail.com>
2022-01-17 22:48:41 +0700

    update train on main branch

--- train.py
+++ train.py
@@ -2,3 +2,7 @@ class Model():
     def __init__(self, hidden_size, n_layers):
 	self.hidden_size = hidden_size
  	self.n_layers = n_layers
+    def fit(self, learning_rate, X, y):
+	model.fit(X, y)
+	return model
+
